cc3edc5e45c756650b5253c6b94de7e4bcbfa576
Akshay Khairnar <khairnaraksh@gmail.com>
2018-05-02 18:17:39 +0530

    first cmmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name:string = '';
+  age:number;
+  found:boolean;
+
+  constructor(private httpClient:HttpClient){  }
+  onNameKeyUp(event:any){
+    this.name = event.target.value;
+    this.found = false;
+  }
+  getProfile(){
+    this.httpClient.get(`http://localhost:3000/posts/?name=${this.name}`)
+    .subscribe(
+      (data:any[]) => {
+        if(data.length) {
+          this.age = data[0].age;
+          this.found = true;
+        }
+      }
+    )
+  }
+
+  postProfile(){
+    this.httpClient.post(`http://localhost:3000/posts`,
+    {
+      name:'Mads',
+      age: 22
+    })
+    .subscribe(
+      (data:any) => {
+        console.log(data);
+      }
+    )
+  }
+
 }
